d28761536cd3e044e3f604d284208d1295c6e84e
Mahesh <123875208+MaheshKanigiri@users.noreply.github.com>
2024-06-04 11:38:29 +0530

    5.Two-way Binding

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -12,6 +12,8 @@ export class ProductComponent implements OnInit{
 
   productImage: string = 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
+  productQuantity: number = 1;
+  
   onBuy() {
     alert(`You bought ${this.productName}!`);
   }
