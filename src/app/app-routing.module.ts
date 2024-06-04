afc380fc0e6af3ccdab6a289f6681b3356e1eaf7
Mahesh <123875208+MaheshKanigiri@users.noreply.github.com>
2024-06-04 22:53:35 +0530

    8.Routing for Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ProductComponent } from './product/product.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerAccessComponent } from './customer-access/customer-access.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/example', pathMatch: 'full' },
+  { path: 'example', component: ExampleComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'customer', component: CustomerComponent },
+  { path: 'customer-access', component: CustomerAccessComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
