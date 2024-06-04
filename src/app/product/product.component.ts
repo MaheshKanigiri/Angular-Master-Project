8bc8cece8d9e962d22181c9c60ef86324ad96dda
Mahesh <123875208+MaheshKanigiri@users.noreply.github.com>
2024-06-04 11:19:57 +0530

    5.Interpolation and PropertyBinding

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,10 +8,15 @@ import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angu
 export class ProductComponent implements OnInit{
   constructor() { }
 
+  productDescription: string = 'This is a great product!';
+
+  productImage: string = 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
   //Parent to Child Communication with @Input
   @Input()
   productName!: string;
 
+  //Child to Parent Comunication emit with @output
   @Output() productClicked: EventEmitter<string> = new EventEmitter<string>();
 
   onClick() {
